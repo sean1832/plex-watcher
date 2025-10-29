3944a1b2fed6e0e75f26313501e8d604e00e0098
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2025-10-29 20:43:00 +1100

    feat: implement manual scan for audiobookshelf

--- backend/internal/api/handler_scan.go
+++ backend/internal/api/handler_scan.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"plexwatcher/internal/http/response"
+	"plexwatcher/internal/services/audiobookshelf"
 	"plexwatcher/internal/services/plex"
 	"plexwatcher/internal/types"
 	"strings"
@@ -22,32 +23,68 @@ func (h *Handler) scan(w http.ResponseWriter, r *http.Request) {
 
 	// Get Plex config from service_configs
 	if plexConfig, ok := req.ServiceConfigs[types.ServicePlex]; ok {
+		logger := slog.With("service", types.ServicePlex)
 		plexClient, err := plex.NewPlexClient(plexConfig.ServerUrl, plexConfig.Token)
 		if err != nil {
 			response.WriteError(w, err.Error(), http.StatusBadRequest)
-			slog.Error("failed to create Plex client", "error", err)
+			logger.Error("failed to create Plex client", "error", err)
 			return
 		}
 		scanner, err := plex.NewScanner(h.Context, plexClient)
 		if err != nil {
 			response.WriteError(w, err.Error(), http.StatusBadRequest)
-			slog.Error("failed to create Plex scanner", "error", err)
+			logger.Error("failed to create Plex scanner", "error", err)
 			return
 		}
-		handlePlexManualScan(h, scanner, &req)
+		handlePlexManualScan(h, scanner, &req, logger)
+		response.WriteSuccess(w, "scanned triggered", nil, http.StatusOK)
+	}
 
+	if absConfig, ok := req.ServiceConfigs[types.ServiceAudiobookshelf]; ok {
+		logger := slog.With("service", types.ServiceAudiobookshelf)
+		absConfig, err := audiobookshelf.NewClient(absConfig.ServerUrl, absConfig.Token)
+		if err != nil {
+			response.WriteError(w, err.Error(), http.StatusBadRequest)
+			logger.Error("failed to create audiobookshelf client", "error", err)
+			return
+		}
+		manager, err := audiobookshelf.NewLibraryManager(h.Context, absConfig)
+		if err != nil {
+			response.WriteError(w, err.Error(), http.StatusBadRequest)
+			logger.Error("failed to create audiobookshelf library manager", "error", err)
+			return
+		}
+		handleAbsManualScan(h, manager, &req, logger)
 		response.WriteSuccess(w, "scanned triggered", nil, http.StatusOK)
 	}
 }
 
-func handlePlexManualScan(h *Handler, scanner *plex.Scanner, req *types.RequestScan) {
-	slog.Info("triggering manual scans", "path_count", len(req.Paths))
+func handleAbsManualScan(h *Handler, libManager *audiobookshelf.LibraryManager, req *types.RequestScan, logger *slog.Logger) {
+	logger.Info("trigger manual scans", "path_count", len(req.Paths))
+	for _, path := range req.Paths {
+		targetDir := filepath.ToSlash(filepath.Dir(path))
+
+		go func(path string, manager *audiobookshelf.LibraryManager) {
+			h.scanSemaphore <- struct{}{}
+			defer func() { <-h.scanSemaphore }()
+			if err := manager.ScanPath(h.Context, path); err != nil {
+				logger.Error("scan failed", "path", path, "error", err)
+			} else {
+				logger.Info("scan completed", "path", path)
+			}
+		}(targetDir, libManager)
+	}
+}
+
+func handlePlexManualScan(h *Handler, scanner *plex.Scanner, req *types.RequestScan, logger *slog.Logger) {
+
+	logger.Info("triggering manual scans", "path_count", len(req.Paths))
 
 	for _, path := range req.Paths {
 		// map to plex path first
 		plexPath, section := scanner.MapToPlexPath(path)
 		if section == nil {
-			slog.Warn("failed to map to any plex library path, skipping scan", "path", path)
+			logger.Warn("failed to map to any plex library path, skipping scan", "path", path)
 			continue
 		}
 
@@ -63,7 +100,7 @@ func handlePlexManualScan(h *Handler, scanner *plex.Scanner, req *types.RequestS
 			targetDir = filepath.Dir(plexPath)
 		} else {
 			// case 3: invalid extension. skip.
-			slog.Warn("disallowed extension found, skipping scan", "path", path, "extension", ext)
+			logger.Warn("disallowed extension found, skipping scan", "path", path, "extension", ext)
 			continue
 		}
 
@@ -74,9 +111,9 @@ func handlePlexManualScan(h *Handler, scanner *plex.Scanner, req *types.RequestS
 			h.scanSemaphore <- struct{}{}        // acquire a token
 			defer func() { <-h.scanSemaphore }() // release the token
 			if section, err := s.ScanPath(h.Context, p); err != nil {
-				slog.Error("scan failed", "path", p, "error", err)
+				logger.Error("scan failed", "path", p, "error", err)
 			} else {
-				slog.Info("scan completed", "path", p, "section", section.SectionTitle)
+				logger.Info("scan completed", "path", p, "section", section.SectionTitle)
 			}
 		}(targetDir, scanner)
 	}
